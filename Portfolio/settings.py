8c8a4c1e02da4154860ec24103c5f1350f019ebb
Jason Peck <pecjas@gmail.com>
2020-01-22 22:25:52 -0600

    Add Google's reCAPTCHA to the contact page.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secrets.key
+GOOGLE_RECAPTCHA_SECRET_KEY = secrets.googleRecaptchaKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .forms import ContactMeForm
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.contrib import messages
 from .models import Project, ProjectImage, Job, JobDetail, Education, Skill
+import requests
 
 # Create your views here.
 def index(request):
@@ -34,20 +34,30 @@ def contact(request):
     if request.method == 'POST':
         form = ContactMeForm(request.POST)
         if form.is_valid():
-            fromEmail = form.cleaned_data.get('fromEmail')
-            message = f"Name: {form.cleaned_data.get('name')}\nEmail: {fromEmail}\nMessage: {form.cleaned_data.get('message')}"
-            email = EmailMessage(form.cleaned_data.get('subject'),
-                                message,
-                                settings.EMAIL_HOST_USER,
-                                ['pecjas@gmail.com'],
-                                headers = {'Reply-To': fromEmail}   )
-            try:
-                email.send()
-                messages.success(request, "Thank you. I will get back to you shortly.")
-                form = ContactMeForm()
-            except:
-                messages.error(request, "Oops, something went wrong. Try again or email me directly at pecjas@gmail.com.")
-            #TODO: Use Toast instead of HttpResponse
+            #Validate google's reCAPTCHA
+            recaptchaResponse = request.POST.get('g-recaptcha-response')
+            data = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptchaResponse
+            }
+            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+            result = r.json()
+            if not result['success']:
+                messages.error(request, "I'm sorry, but you have failed the reCAPTCHA verification. No email was sent.")
+            else:
+                fromEmail = form.cleaned_data.get('fromEmail')
+                message = f"Name: {form.cleaned_data.get('name')}\nEmail: {fromEmail}\nMessage: {form.cleaned_data.get('message')}"
+                email = EmailMessage(form.cleaned_data.get('subject'),
+                                    message,
+                                    settings.EMAIL_HOST_USER,
+                                    ['pecjas@gmail.com'],
+                                    headers={'Reply-To': fromEmail}   )
+                try:
+                    email.send()
+                    messages.success(request, "Thank you. I will get back to you shortly.")
+                    form = ContactMeForm()
+                except:
+                    messages.error(request, "Oops, something went wrong. Try again or email me directly at pecjas@gmail.com.")
     else:
         form = ContactMeForm()
     return render(request, 
